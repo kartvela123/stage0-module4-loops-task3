eaa2a5d77be3714a9e7a51fb22e03b1ead256ea7
kart <mkartveli9@gmail.com>
2023-02-22 01:17:21 +0000

    4.2

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -4,12 +4,14 @@ public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
         int newnum = 1;
         int prev = 0;
+        int res = 0;
         System.out.println(prev);
         System.out.println(newnum);
         for (int num = 3; num < lastFibonacci; num++) {
             System.out.println(newnum + prev);
+            res = newnum + prev;
             prev = newnum;
-            newnum = newnum + prev;
+            newnum = res;
         }
     }
 }
